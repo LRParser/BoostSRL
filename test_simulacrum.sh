51b93c28f6535566b68b93e98847107c739dcc22
Joe <jth82@drexel.edu>
2019-03-03 23:50:56 +0100

    Latest simulacrum test

--- test_simulacrum.sh
+++ test_simulacrum.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-java -cp code/target/BoostSRL-1.0-SNAPSHOT.jar edu.wisc.cs.will.Boosting.RDN.RunBoostedRDN -l -train ~/Downloads/simulacrum/train -target diedofcancer > train.log 
+java -cp code/target/BoostSRL-1.0-SNAPSHOT.jar edu.wisc.cs.will.Boosting.RDN.RunBoostedRDN -l -train ~/Downloads/simulacrum/train -trees 10 -target diedofcancer >> train.log 
 
-java -cp code/target/BoostSRL-1.0-SNAPSHOT.jar edu.wisc.cs.will.Boosting.RDN.RunBoostedRDN -i -model ~/Downloads/simulacrum/train/models -test ~/Downloads/simulacrum/test -target diedofcancer -aucJarPath . > test.log
+java -cp code/target/BoostSRL-1.0-SNAPSHOT.jar edu.wisc.cs.will.Boosting.RDN.RunBoostedRDN -i -model ~/Downloads/simulacrum/train/models -trees 10 -test ~/Downloads/simulacrum/test -target diedofcancer -aucJarPath . >> test.log
